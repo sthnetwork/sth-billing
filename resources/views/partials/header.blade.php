799273fba02261e612f2ea352f63701a79acab81
sthnetwork <ujikmatthew@gmail.com>
2025-06-14 03:22:46 +0700

    Create header.blade.php

---
+++ resources/views/partials/header.blade.php
@@ -0,0 +1,21 @@
+<meta charset="UTF-8">
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
+<meta http-equiv="X-UA-Compatible" content="ie=edge">
+<title>Billing STHNetwork</title>
+
+<!-- Fonts -->
+<link rel="preconnect" href="https://fonts.googleapis.com">
+<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+<link href="https://fonts.googleapis.com/css2?family=Roboto:wght@300;400;500;700;900&display=swap" rel="stylesheet">
+
+<!-- Core CSS -->
+<link rel="stylesheet" href="{{ asset('assets/vendors/core/core.css') }}">
+
+<!-- Plugin CSS -->
+<link rel="stylesheet" href="{{ asset('assets/vendors/flatpickr/flatpickr.min.css') }}">
+
+<!-- Layout CSS -->
+<link rel="stylesheet" href="{{ asset('assets/css/demo1/style.css') }}">
+
+<!-- Favicon -->
+<link rel="shortcut icon" href="{{ asset('assets/images/favicon.png') }}" />
